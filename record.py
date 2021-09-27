1ea9861b16c7caca695eac43e28c2cc1a57ed033
benkajaja <ny343478@gmail.com>
2021-09-27 21:15:17 +0800

    Rename variable `targetGPU`

--- record.py
+++ record.py
@@ -18,13 +18,13 @@ time.sleep(1)
 cur_bits_recv = psutil.net_io_counters(pernic=True)[targetNIC].bytes_recv * 8
 cur_bits_sent = psutil.net_io_counters(pernic=True)[targetNIC].bytes_sent * 8
 
-GPUtarget = GPUtil.getGPUs()[0]
+targetGPU = GPUtil.getGPUs()[0]
 
 log = "[DEBUG] STATS. CPU: %.2f, MEM: %.2f, GPU: %.2f, GPUMEM: %.2f, UPLINK: %.2f, DOWNLINK: %.2f" % (
     psutil.cpu_percent()/100,
     psutil.virtual_memory()[2]/100,
-    GPUtarget.load,
-    GPUtarget.memoryUtil,
+    targetGPU.load,
+    targetGPU.memoryUtil,
     (cur_bits_sent - pre_bits_sent) /(1024), # in Kbps
     (cur_bits_recv - pre_bits_recv) /(1024) # in Kbps
 )
